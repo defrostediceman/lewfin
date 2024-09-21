8c020ae998e4ed8f336ad48731eb3efb3660cb41
defrostediceman <78688595+defrostediceman@users.noreply.github.com>
2024-09-21 23:04:28 +0100

    Update build.sh
    Added cockpit & all plugins to build.

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen tmux mosh qemu-kvm crun-vm
+rpm-ostree install screen tmux mosh qemu-kvm crun-vm cockpit*
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
