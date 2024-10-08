9741dfe6f0047843c49d1f206fe4a25e24fe4caf
defrostediceman <78688595+defrostediceman@users.noreply.github.com>
2024-10-08 10:28:16 +0100

    Update build.sh
    Include install of crun & cockpit-podman. 
    
    Include systemd enabling of cockpit.socket. This could be changed to cockpit-ws container in the future.

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ RELEASE="$(rpm -E %fedora)"
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-rpm-ostree install screen tmux mosh qemu-kvm crun-vm cockpit*
+rpm-ostree install screen tmux mosh qemu-kvm crun crun-vm cockpit cockpit-podman
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
@@ -21,3 +21,4 @@ rpm-ostree install screen tmux mosh qemu-kvm crun-vm cockpit*
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable cockpit.socket
